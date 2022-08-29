6020b219e20f139ef18c2ccfe3abd56762f4d17b
Jonathas Soares <joul.gs@gmail.com>
2022-08-29 10:30:36 -0300

    rotas comentadas

--- src/api/routes/group.route.js
+++ src/api/routes/group.route.js
@@ -5,19 +5,20 @@ const loginVerify = require('../middlewares/loginCheck')
 
 const router = express.Router()
 
-router.route('/create').post(keyVerify, loginVerify, controller.create)
-router.route('/listall').get(keyVerify, loginVerify, controller.listAll)
-router.route('/leave').get(keyVerify, loginVerify, controller.leaveGroup)
+// router.route('/create').post(keyVerify, loginVerify, controller.create)
+// router.route('/listall').get(keyVerify, loginVerify, controller.listAll)
+// router.route('/leave').get(keyVerify, loginVerify, controller.leaveGroup)
 
-router
-    .route('/inviteuser')
-    .post(keyVerify, loginVerify, controller.addNewParticipant)
-router.route('/makeadmin').post(keyVerify, loginVerify, controller.makeAdmin)
-router
-    .route('/demoteadmin')
-    .post(keyVerify, loginVerify, controller.demoteAdmin)
-router
-    .route('/getinvitecode')
-    .get(keyVerify, loginVerify, controller.getInviteCodeGroup)
+// router
+//     .route('/inviteuser')
+//     .post(keyVerify, loginVerify, controller.addNewParticipant)
+// router.route('/makeadmin').post(keyVerify, loginVerify, controller.makeAdmin)
+// router
+//     .route('/demoteadmin')
+//     .post(keyVerify, loginVerify, controller.demoteAdmin)
+// router
+//     .route('/getinvitecode')
+//     .get(keyVerify, loginVerify, controller.getInviteCodeGroup)
+// router.route('/getallgroups').get(keyVerify, loginVerify, controller.getAllGroups)
 
 module.exports = router
