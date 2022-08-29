b86168e88d47ec56d0f8dcfc7305829f91ec7834
Jonathas Soares <joul.gs@gmail.com>
2022-08-29 11:25:11 -0300

    rotas comentadas e validação token

--- src/api/routes/misc.route.js
+++ src/api/routes/misc.route.js
@@ -6,8 +6,8 @@ const loginVerify = require('../middlewares/loginCheck')
 const router = express.Router()
 
 router.route('/onwhatsapp').get(keyVerify, loginVerify, controller.onWhatsapp)
-router.route('/downProfile').get(keyVerify, loginVerify, controller.downProfile)
-router.route('/getStatus').get(keyVerify, loginVerify, controller.getStatus)
-router.route('/blockUser').get(keyVerify, loginVerify, controller.blockUser)
+// router.route('/downProfile').get(keyVerify, loginVerify, controller.downProfile)
+// router.route('/getStatus').get(keyVerify, loginVerify, controller.getStatus)
+// router.route('/blockUser').get(keyVerify, loginVerify, controller.blockUser)
 
 module.exports = router
